4e894b9c4dcee74246ebe79a51427c2f7aaa5073
SomefunAgba <oasomefun@futa.edu.ng>
2021-03-23 11:32:52 +0100

    update app test functionality to reflect no-display mode

--- tests/testAppUpdnModel.m
+++ tests/testAppUpdnModel.m
@@ -1,8 +1,10 @@
 classdef testAppUpdnModel < matlab.uitest.TestCase
     % testAppUpdnModel: Unit Test Suite.
-    %   Failure means something is wrong with App    
+    %   Failure should mean something is wrong with App
     properties
         App
+        % check if a display is present for this test-suite
+        istext = ~usejava('jvm') || ~feature('ShowFigureWindows');
     end
     
     
@@ -18,17 +20,21 @@ classdef testAppUpdnModel < matlab.uitest.TestCase
             addpath(genpath(rootpath));
             if isfolder(fullfile(rootpath,'bin'))
                 rmpath(fullfile(rootpath,"bin"))
-            end
-            
+            end       
         end
     end
     
     methods (TestMethodSetup)
         function launchApp(testCase)
-            %import matlab.unittest.diagnostics.ScreenshotDiagnostic 
+            %import matlab.unittest.diagnostics.ScreenshotDiagnostic
             %testCase.onFailure(ScreenshotDiagnostic);
-            testCase.App = covid19nlsigApp;
-            testCase.addTeardown(@delete,testCase.App);
+            
+            if ~testCase.istext
+                testCase.App = covid19nlsigApp;
+                testCase.addTeardown(@delete,testCase.App);
+            else
+                testCase.assertTrue(testCase.istext);
+            end
         end
     end
     
@@ -39,34 +45,42 @@ classdef testAppUpdnModel < matlab.uitest.TestCase
                 'NLSIG-COVID19Lab requires Matlab R2020a or later');
         end
         
-        function test_UpdButton(testCase)          
+        function test_UpdButton(testCase)
             % press and verify update button
-            testCase.press(testCase.App.UpdateDatabaseButton)
-            testCase.verifyEqual(testCase.App.dataupdated,1);
+            if ~testCase.istext
+                testCase.press(testCase.App.UpdateDatabaseButton)
+                testCase.verifyEqual(testCase.App.dataupdated,1);
+            else
+                testCase.assertTrue(testCase.istext);
+            end
         end
         
         function test_ModelButton(testCase)
             % choose and verify country-code
-            testCase.verifyEqual(testCase.App.SearchCodesDropDown.Value,'WD')
-            testCase.choose(testCase.App.SearchCodesDropDown,'US')
-            testCase.verifyEqual(testCase.App.SearchCodesDropDown.Value,'US')
-            testCase.choose(testCase.App.SearchCodesDropDown,'GB')
-            testCase.verifyEqual(testCase.App.SearchCodesDropDown.Value,'GB')
-            
-            % choose epidemic type
-            testCase.choose(testCase.App.DeathsButton)
-            testCase.verifyTrue(testCase.App.DeathsButton.Value)
-            %            
-            testCase.choose(testCase.App.InfectionsButton)
-            testCase.verifyTrue(testCase.App.InfectionsButton.Value)
-            
-            % type/pick a stop-date
-            testCase.type(testCase.App.StopDateDatePicker,datetime(2020,05,01))
-            testCase.verifyEqual(testCase.App.StopDateDatePicker.Value,datetime(2020,05,01))          
-            
-            % press and verify model button
-            testCase.press(testCase.App.ModelButton)
-            testCase.verifyEqual(testCase.App.modelran,1);
+            if ~testCase.istext
+                testCase.verifyEqual(testCase.App.SearchCodesDropDown.Value,'WD')
+                testCase.choose(testCase.App.SearchCodesDropDown,'US')
+                testCase.verifyEqual(testCase.App.SearchCodesDropDown.Value,'US')
+                testCase.choose(testCase.App.SearchCodesDropDown,'GB')
+                testCase.verifyEqual(testCase.App.SearchCodesDropDown.Value,'GB')
+                
+                % choose epidemic type
+                testCase.choose(testCase.App.DeathsButton)
+                testCase.verifyTrue(testCase.App.DeathsButton.Value)
+                %
+                testCase.choose(testCase.App.InfectionsButton)
+                testCase.verifyTrue(testCase.App.InfectionsButton.Value)
+                
+                % type/pick a stop-date
+                testCase.type(testCase.App.StopDateDatePicker,datetime(2020,05,01))
+                testCase.verifyEqual(testCase.App.StopDateDatePicker.Value,datetime(2020,05,01))
+                
+                % press and verify model button
+                testCase.press(testCase.App.ModelButton)
+                testCase.verifyEqual(testCase.App.modelran,1);
+            else
+                testCase.assertTrue(testCase.istext);
+            end
         end
         
     end
